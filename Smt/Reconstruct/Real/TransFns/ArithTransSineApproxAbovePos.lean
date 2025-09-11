88b48037d297d38f7277245f9aac3cc46b646ecf
Tomaz Gomes Mascarenhas <tomgm1502@gmail.com>
2025-09-11 18:31:24 -0300

    Add corrected version of SINE APPROX ABOVE POS
    Co-authored-by: Harun Khan <hacker.hk19@gmail.com>

--- Smt/Reconstruct/Real/TransFns/ArithTransSineApproxAbovePos.lean
+++ Smt/Reconstruct/Real/TransFns/ArithTransSineApproxAbovePos.lean
@@ -16,15 +16,23 @@ open Set Real
 
 namespace Smt.Reconstruct.Real.TransFns
 
-theorem arithTransSineApproxAbovePos (d k : ℕ) (x : Real) (hd : d = 2*k + 1)
-                                     (hx : 0 < x) :
-    let p : ℕ → ℝ → ℝ := fun d x => taylorWithinEval Real.sin d Set.univ 0 x + x ^ (d + 1) / (d + 1).factorial
-    Real.sin x ≤ p d x := by
-  intro p
-  simp [p]
+theorem arithTransSineApproxAbovePos' (d k : ℕ) (x : ℝ) (hd : d = 2*k + 1)
+                                     (hx1 : 0 ≤ x) :
+    Real.sin x ≤ taylorWithinEval Real.sin d Set.univ 0 x + x ^ (d + 1) / (d + 1).factorial := by
   rw [← neg_neg x, sin_neg, taylorSin_neg, neg_add_eq_sub, ←neg_sub, neg_le_neg_iff, Even.neg_pow (by rw [hd]; norm_num)]
-  apply arithTransSineApproxBelowNeg d k
-  · linarith
-  · linarith
+  apply arithTransSineApproxBelowNeg d k hd (by linarith)
+
+theorem arithTransSineApproxAbovePos (d k : ℕ) (lb ub x c : ℝ) (hd : d = 2*k + 1)
+                                     (hl : 0 ≤ lb) (hu : ub ≤ π)
+                                     (hx1 : lb ≤ x) (hx2 : x ≤ ub)
+                                     (hc : c = if ub < π/2 then ub else if lb < π/2 then π/2 else lb) :
+    Real.sin x ≤ taylorWithinEval Real.sin d Set.univ 0 c + c ^ (d + 1) / (d + 1).factorial := by
+  have : 0 ≤ c := by split_ifs at hc <;> linarith
+  apply le_trans _ (arithTransSineApproxAbovePos' d k c hd this)
+  split_ifs at hc with h1 h2
+  · apply sin_le_sin_of_le_of_le_pi_div_two <;> linarith
+  · rw [hc, sin_pi_div_two]; apply sin_le_one
+  · rw [← sin_pi_sub, ← sin_pi_sub c]
+    apply sin_le_sin_of_le_of_le_pi_div_two <;> linarith
 
 end Smt.Reconstruct.Real.TransFns
--- Smt/Reconstruct/Real/TransFns/ArithTransSineApproxBelowNeg.lean
+++ Smt/Reconstruct/Real/TransFns/ArithTransSineApproxBelowNeg.lean
@@ -16,17 +16,30 @@ open Set Real
 
 namespace Smt.Reconstruct.Real.TransFns
 
-theorem arithTransSineApproxBelowNeg (d k : Nat) (x : Real) (hd : d =2*k + 1) (hx : x < 0) :
-  let p : ℕ → ℝ → ℝ := fun d x => taylorWithinEval Real.sin d Set.univ 0 x - x ^ (d + 1) / (d + 1).factorial
-  p d x ≤ sin x := by
-  intro p; simp only [p]
-  have ⟨x', hx', H⟩ := taylor_mean_remainder_lagrange₁ (n := d) hx contDiff_sin
-  rw [taylorWithinEval_eq _ (right_mem_Icc.mpr (le_of_lt hx)) (uniqueDiffOn_Icc hx) (contDiff_sin)] at H
-  rw [←sub_nonneg, ←sub_add, H]
-  rw [iteratedDerivWithin_eq_iteratedDeriv contDiff_sin (uniqueDiffOn_Icc hx) _ (Ioo_subset_Icc_self hx')]
-  rw [sub_zero, mul_div_assoc, ← add_one_mul]
-  apply mul_nonneg _ _
-  · rw [←neg_le_iff_add_nonneg', neg_le]; apply neg_one_le_iteratedDeriv_sin
-  · apply mul_nonneg (le_of_lt (Even.pow_pos (by rw [hd]; norm_num) (by linarith))) (by simp [Nat.factorial_pos (d + 1)])
+theorem arithTransSineApproxBelowNeg (d k : Nat) (hd : d = 2*k + 1) (hx : x ≤ 0) :
+  taylorWithinEval Real.sin d Set.univ 0 x - x ^ (d + 1) / (d + 1).factorial ≤ sin x := by
+  cases' lt_or_eq_of_le hx with hx hx
+  · have ⟨x', hx', H⟩ := taylor_mean_remainder_lagrange₁ (n := d) hx contDiff_sin
+    rw [taylorWithinEval_eq _ (right_mem_Icc.mpr (le_of_lt hx)) (uniqueDiffOn_Icc hx) (contDiff_sin)] at H
+    rw [←sub_nonneg, ←sub_add, H]
+    rw [iteratedDerivWithin_eq_iteratedDeriv contDiff_sin (uniqueDiffOn_Icc hx) _ (Ioo_subset_Icc_self hx')]
+    rw [sub_zero, mul_div_assoc, ← add_one_mul]
+    apply mul_nonneg _ _
+    · rw [←neg_le_iff_add_nonneg', neg_le]; apply neg_one_le_iteratedDeriv_sin
+    · apply mul_nonneg (le_of_lt (Even.pow_pos (by rw [hd]; norm_num) (by linarith))) (by simp [Nat.factorial_pos (d + 1)])
+  · simp [hx]
+
+/- theorem arithTransSineApproxBelowNeg (d k : Nat) (x : Real) (hd : d =2*k + 1) (hx : x < 0) : -/
+/-   let p : ℕ → ℝ → ℝ := fun d x => taylorWithinEval Real.sin d Set.univ 0 x - x ^ (d + 1) / (d + 1).factorial -/
+/-   p d x ≤ sin x := by -/
+/-   intro p; simp only [p] -/
+/-   have ⟨x', hx', H⟩ := taylor_mean_remainder_lagrange₁ (n := d) hx contDiff_sin -/
+/-   rw [taylorWithinEval_eq _ (right_mem_Icc.mpr (le_of_lt hx)) (uniqueDiffOn_Icc hx) (contDiff_sin)] at H -/
+/-   rw [←sub_nonneg, ←sub_add, H] -/
+/-   rw [iteratedDerivWithin_eq_iteratedDeriv contDiff_sin (uniqueDiffOn_Icc hx) _ (Ioo_subset_Icc_self hx')] -/
+/-   rw [sub_zero, mul_div_assoc, ← add_one_mul] -/
+/-   apply mul_nonneg _ _ -/
+/-   · rw [←neg_le_iff_add_nonneg', neg_le]; apply neg_one_le_iteratedDeriv_sin -/
+/-   · apply mul_nonneg (le_of_lt (Even.pow_pos (by rw [hd]; norm_num) (by linarith))) (by simp [Nat.factorial_pos (d + 1)]) -/
 
 end Smt.Reconstruct.Real.TransFns
